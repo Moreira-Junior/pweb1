a674dc594b342ac83d33f39af45cbaebba6f531f
Moreira-Junior <junior_moreira88@hotmail.com>
2022-09-08 16:12:39 -0300

    adding ts practice

---
+++ PraticaBanco-Ts/app/ts/script.ts
@@ -0,0 +1,44 @@
+const c1 = new Conta('1', 100);
+const c2 = new Conta('2');
+
+
+console.log(c1.numero);
+const contaController = new ContaController();
+contaController.adicionarConta(c1);
+contaController.adicionarConta(c2);
+contaController.listar();
+
+const contaBonificada = new ContaBonificada('10', 100);
+contaBonificada.creditar(10);
+console.log(contaBonificada.getSaldo());
+
+const p1 = new PessoaFisica('gustavo', 25, new Date(14/5/1997), '1234');
+const p2 = new PessoaJuridica('ifpb', 30, new Date(14/5/1992), '1234');
+const p3 = new Pessoa('alguem', 35, new Date(24/6/87));
+console.log(p1.nome);
+console.log(p2.nome);
+console.log(p3.nome);
+
+// const repContas = new RepositorioContas();
+//
+// const c1 = new Conta('1', 100);
+// const c2 = new Conta('2');
+//
+// repContas.adicionar(c1);
+// repContas.adicionar(c2);
+//
+// repContas.getContas().forEach(conta => console.log(conta.numero))
+
+// console.log(c1.saldo)
+// console.log(c2.saldo)
+//
+// c1.saldo = 200;
+//
+// c1.debitar(50);
+// c2.creditar(1000);
+// console.log(c1.saldo)
+// console.log(c2.saldo)
+//
+// c2.transferir(c1, 500);
+// console.log(c1.saldo)
+// console.log(c2.saldo)
